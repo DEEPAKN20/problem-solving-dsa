56bb8d574f44acaa794a79d76f781873982fe755
DEEPAKN20 <deepakn94496@gmail.com>
2026-09-15 18:15:14 +0530

    Concatination of Array

---
+++ 1929.Concatination of Array.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/concatenation-of-array/description/
+#https://leetcode.com/submissions/detail/2142582158/
+
+# 15 September 2026 - 2 Mins
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans=2*nums
+        return ans
+        
+
+        
