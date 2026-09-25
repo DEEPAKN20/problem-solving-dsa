032550641fd6812ba253d18a67d888558a5473ec
DEEPAKN20 <deepakn94496@gmail.com>
2026-09-25 22:29:58 +0530

    709. To Lower Case

---
+++ 709. To Lower Case.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/to-lower-case/
+#https://leetcode.com/submissions/detail/2153151930/
+
+#25 September 2026 - 1 Mins
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+"""
+"""
